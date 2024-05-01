be99aeda8f3089c7468d243401a91a0fdaf5ef08
Shreeya Shrestha <shresthashreeya788@gmail.com>
2024-05-01 19:19:16 +0545

    Added assignments folder with Remember-event-project

---
+++ Assignments/remember_event/remember_event_project/event_manager/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Event(models.Model):
+    recipient_email = models.EmailField()
+    scheduled_datetime = models.DateTimeField()
+    message_content = models.TextField()
+
+    def __str__(self):
+        return f"{self.recipient_email} - {self.scheduled_datetime}"
+
+
+
