da8b9c3ed1cbd718984cf3878a630dba6292c263
Prinsa Joshi <59060734+prinsajoshi@users.noreply.github.com>
2024-05-02 23:14:25 +0545

    image_classifier

---
+++ Projects/identify_image/frontend_image/myimageapp/src/App.jsx
@@ -0,0 +1,11 @@
+import InputForm from './components/inputForm.jsx';
+
+function App() {
+  return (
+    <div className='container_app'>
+    <InputForm/>
+    </div>
+  );
+}
+
+export default App;
