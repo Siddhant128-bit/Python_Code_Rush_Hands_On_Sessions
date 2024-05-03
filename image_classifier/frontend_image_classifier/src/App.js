ddf2ef1c8b1ec9e37dcd042f25d9906dbb8b9736
dawadi_pratima <dawadipratima72@gmail.com>
2024-05-03 20:04:37 +0545

    image classifier project modified

---
+++ image_classifier/frontend_image_classifier/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import ImageUpload from './ImageUpload';
+
+function App() {
+  return (
+    <div className="App">
+    <header className="App-header">
+      <h1>Image Classifier Project</h1>
+    </header>
+    <main>
+      <ImageUpload />
+    </main>
+  </div>
+  );
+}
+
+export default App;
